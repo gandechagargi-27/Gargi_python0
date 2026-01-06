1f5a86e0308b18648d5d5b4051c9361021e0f87b
GandechaGargi <gandechagargi@gmail.com>
2026-01-06 07:48:58 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,32 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print('helloworld')
+helloworld
+a = 27
+print(a)
+27
+b = 27.5
+print(b)
+27.5
+>>> c =["apple","banana","cherry"]
+>>> print(c)
+['apple', 'banana', 'cherry']
+>>> d =("apple","banana","cherry")
+>>> print(d)
+('apple', 'banana', 'cherry')
+>>> e = True
+>>> print(e)
+True
+>>> f = False
+>>> print(f)
+False
+>>> Falseg+False
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    Falseg+False
+NameError: name 'Falseg' is not defined. Did you mean: 'False'?
+
+>>> 
+>>> 
+>>> 
+>>> 
