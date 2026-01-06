e1937a9b2da27329d77cb3f02dce5b1c4ee7f37a
GandechaGargi <gandechagargi@gmail.com>
2026-01-06 07:49:58 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,48 @@
+#Arithmetic
+x = 10
+y = 20
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+#Assignment
+x = 27
+print(10)
+
+#Unary Minus
+a=27
+print(-a)
+
+#Relational 
+p=20
+q=40
+print(p<q)
+print(p==q)
+
+#logical
+x=True
+y=False
+print(x and y)
+print(x or y)
+
+#boolean
+is_pass=True
+print(is_pass)
+
+#bitwise
+a=5
+b=5
+print(a&b)
+print(a|b)
+
+#membership
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(3 not in numbers)
+
+#identity
+x=10
+y=10
+print(x is y)
+print(x is not y)
